d80672a6bc8d4508daa02bfabe024b4194fd5e54
User1 <User1@LP-5CD9176M6P.HCLT.CORP.HCL.IN>
2019-08-26 14:10:23 +0530

    Entity for stock and cmpmarket

---
+++ src/main/java/com/stock/app/entiry/CMPMarket.java
@@ -0,0 +1,26 @@
+package com.stock.app.entiry;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+@Data
+@Entity
+@Table
+public class CMPMarket {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int cmpId;
+	private double marketPrice;
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalDate dateTime;
+	private int stockId;
+	}
---
+++ src/main/java/com/stock/app/entiry/Stock.java
@@ -0,0 +1,26 @@
+package com.stock.app.entiry;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table
+public class Stock {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int stockId;
+	
+	private String stockName;
+	private double price;
+	private int quantity;
+	private String trending;
+	private String stockExchangeName;
+
+}
